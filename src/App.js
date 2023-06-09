3429c120ded569d6277da6621793a620bc2bb3c2
Hussein Samy <hussein.samy02@gmail.com>
2023-06-09 20:10:48 +0200

    Adds main application routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Header from './components/header/header.component';
+import Home from './routes/home/home.component';
+import Shops from './routes/shops/shops.component';
+import {Routes, Route} from 'react-router-dom'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Header />}>
+        <Route index element={<Home/>} />
+        <Route path='home' element={<Home/>} />
+        <Route path='shop' element={<Shops/>} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
