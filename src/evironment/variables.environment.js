3126c68a3ce279d326090284344214255b62813d
Hussein Samy <hussein.samy02@gmail.com>
2023-06-10 18:14:53 +0200

    Creates enivronment varaibles

---
+++ src/evironment/variables.environment.js
@@ -0,0 +1,10 @@
+const env = {
+    // Firebase configuration
+    apiKey: "AIzaSyAdI7CSIyH9R5F4sbDtE2LihxXlSjmfx3s",
+    authDomain: "crwn-clothing-db-ba6dc.firebaseapp.com",
+    projectId: "crwn-clothing-db-ba6dc",
+    storageBucket: "crwn-clothing-db-ba6dc.appspot.com",
+    messagingSenderId: "917721012346",
+    appId: "1:917721012346:web:020a788fed4eb3d4f143c3"
+};
+export default env; 
