e795f7dcd2e1d1a3d907864a250fe0def48ce25b
Hussein Samy <hussein.samy02@gmail.com>
2023-06-10 18:15:24 +0200

    Adds firebase google authentication button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,14 @@
+import { siginInWithGooglePopup } from '../../utils/firebase/firebase.utils'
 const SignIn = () => {
+    const signInWithGoogle = async () => {
+        const response = await siginInWithGooglePopup();
+        console.log(response)
+    } 
     return (
-        <div>helow from the sign in component</div>
+        <div>
+            <h1>helow from the sign in component</h1>
+            <button onClick={signInWithGoogle}>Sign in with google</button>
+        </div>
     )
 }
 
